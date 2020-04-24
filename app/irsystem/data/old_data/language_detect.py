abe9de98decebd12009b172450cb2f8a5a7eae9d
einc <ein.chang@gmail.com>
2020-04-23 20:47:17 -0700

    remove ads

--- app/irsystem/data/old_data/language_detect.py
+++ app/irsystem/data/old_data/language_detect.py
@@ -3,6 +3,7 @@ import json
 from langdetect import detect
 from langdetect import DetectorFactory
 DetectorFactory.seed = 0
+import numpy as np
 
 def load_json_file(name):
     current_directory = os.path.dirname(os.path.realpath(__file__))
@@ -17,14 +18,22 @@ def diff_langs(data):
   # lang_counts = dict()
   # lang_counts['unclassified'] = 0
 
-  articles = []
+  articles1 = []
+  articles2 = []
+
+  split = True
 
   for post in data:
     transcript = post['transcript']
     try:
       lang = detect(transcript)
       if lang == 'en':
-        articles.append(lang)
+        if split:
+          articles1.append(post)
+          split = False
+        else:
+          articles2.append(post)
+          split = True
       # if lang not in lang_counts:
       #   lang_counts[lang] = 1
       # else:
@@ -34,20 +43,23 @@ def diff_langs(data):
       # print(len(transcript))
       # lang_counts['unclassified'] += 1
 
-  with open('f_data.json', 'w') as json_file:
-    json.dump(articles, json_file)
+
+  with open('HALF1.json', 'w') as json_file:
+      json.dump(articles1, json_file)
+  with open('HALF2.json', 'w') as json_file:
+      json.dump(articles2, json_file)
 
 def main():
   # Load in data
-    # data_file_name = "lang.json"
-    # data = load_json_file(data_file_name)
-    # lang_freq = diff_langs(data)
+  data_file_name = "lang.json"
+  data = load_json_file(data_file_name)
+  lang_freq = diff_langs(data)
 
     # print(lang_freq)
     # print(len(lang_freq))
-  data_file_name = "f_data.json"
-  data = load_json_file(data_file_name)
-  print(len(data))
+  # data_file_name = "f_data.json"
+  # data = load_json_file(data_file_name)
+  # print(len(data))
 
 if __name__ == "__main__":
     main()
