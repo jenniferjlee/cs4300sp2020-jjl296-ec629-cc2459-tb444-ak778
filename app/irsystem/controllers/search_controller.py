1e2fb78cf8e2f0ffd590954a0bd1c6be4d9fa672
spaincam8 <ccedenotobon47@gmail.com>
2020-04-26 14:48:25 -0400

    storing search results in dictionary

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,9 +10,16 @@ project_name = "CUSmiles"
 net_id = "Jennifer Lee: jjl296, Camilo Cedeno-Tobon: cc2459, Tanmay Bansal: tb444, Alina Kim: ak778, Ein Chang: ec629 "
 
 documents = load_json_file('final_data_url.json')
-inverted_index = load_json_file('transcript_inverted_index.json')
-tfidf_matrix = load_json_file('transcript_tfidf_matrix.json')
-norms = load_json_file('transcript_norms.json')
+
+transcript_idf_values = load_json_file('transcript_idf_values.json')
+transcript_inverted_index = load_json_file('transcript_inverted_index.json')
+#transcript_tfidf_matrix = load_json_file('transcript_tfidf_matrix.json')
+transcript_norms = load_json_file('transcript_norms.json')
+
+title_idf_values = load_json_file('title_idf_values.json')
+title_inverted_index = load_json_file('title_inverted_index.json')
+#title_tfidf_matrix = load_json_file('title_tfidf_matrix.json')
+title_norms = load_json_file('title_norms.json')
 
 
 
@@ -25,11 +32,13 @@ def search():
         url = []
         output_message = 'No results'
     else:
-        results = search_tfdf_method(query, tfidf_matrix, inverted_index, norms, tokenize)
-        top_5 = get_top_k(results, 5, documents)
+        transcript_results = search_tfdf_method(query, transcript_inverted_index, transcript_norms, transcript_idf_values, tokenize)
+        title_results = search_tfdf_method(query, title_inverted_index, title_norms, title_idf_values, tokenize)
+        combined_results = get_combined_results(transcript_results, title_results, 0.4, 0.6)
+        top_results = get_top_k(combined_results, 10, documents)
         output_message = "Your search: " + query
-        data = top_5
-        if (len(top_5)==0):
-            data = [('No Results Found', '')]
+        data = top_results
+        if (len(data)==0):
+            data = [{'title':'No Results Found', 'url':''}]
     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -5,31 +5,81 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer 
 import re
 
-def search_tfdf_method(query, input_tfidf_matrix, input_inverted_index, input_doc_norms, tokenize_method):
-    n_docs = len(input_tfidf_matrix)
+def search_tfdf_method(query, input_inverted_index, 
+        input_doc_norms, input_idf_values, tokenize_method):
+    n_docs = len(input_doc_norms)
     q_tokens = tokenize_method(query)
     totals = np.zeros(n_docs)
+    query_norm = 0
     for t in set(q_tokens):
+        query_df = q_tokens.count(t)
         if (input_inverted_index.get(t) is not None):
             for tup in input_inverted_index.get(t):
                 doc_id = tup[0]
-                totals[doc_id] += input_tfidf_matrix[doc_id][t]
+                doc_df = tup[1]
+                idf = input_idf_values[t]
+                totals[doc_id] += doc_df*idf*query_df*idf
+                query_norm += math.pow(query_df*idf, 2)
     sim = []
+    query_norm = math.sqrt(query_norm)
     for i in range(n_docs):
         val = 0
-        if (input_doc_norms[i] != 0):
-            val = totals[i]/(input_doc_norms[i])
+        if ((input_doc_norms[i] != 0) & (query_norm != 0)):
+            val = totals[i]/(input_doc_norms[i] * query_norm)
         if(val > 0):
             sim.append((val, i))
-    sim.sort(key = lambda x: x[0], reverse = True)
     return sim
 
+
 def get_top_k(results, k, input_data):
+    results.sort(key = lambda x: x[0], reverse = True)
     output = []
     for i in range(min(k, len(results))):
         doc_id = results[i][1]
-        title = (input_data[doc_id]['title'], input_data[doc_id].get('url'))
-        output.append(title)
+        doc_info = {'title' : input_data[doc_id].get('title'), 'url': input_data[doc_id].get('url'),
+                    'date':input_data[doc_id].get('date'), 'score':input_data[doc_id].get('score'),
+                    'source':input_data[doc_id].get('source')}
+        output.append(doc_info)
     return output
+
+def sort_by_recency(results, input_data):
+    results.sort(key = lambda x: x['date'], reverse = True)
+    return results
+
+def sort_by_popularity(results, input_data):
+    results.sort(key = lambda x: x['score'], reverse = True)
+    return results
+
+
+def get_combined_results(output_A, output_B, weight_A, weight_B):
+    i = 0
+    j = 0
+    combined_output = []
+    if (len(output_A)==0):
+        return output_B
+    if (len(output_B)==0):
+        return output_A
+    while ((i < len(output_A)) & (j < len(output_B))):
+        doc_A = output_A[i][1]
+        doc_B = output_B[j][1]
+        score_A = output_A[i][0]
+        score_B = output_B[j][0]
+        if (doc_A == doc_B):
+            combined_score = weight_A*score_A + weight_B*score_B
+            combined_output.append((combined_score, doc_A))
+            i+=1
+            j+=1
+        elif (doc_A < doc_B):
+            combined_output.append((weight_A*score_A, doc_A))
+            i+=1
+        else:
+            combined_output.append((weight_B*score_B, doc_B))
+            j+=1
+    return combined_output
+
+
+
+
+
 
 
