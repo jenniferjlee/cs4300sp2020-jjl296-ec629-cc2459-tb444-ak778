0a60b84f16d9c6ad0637a9956e296d864e341365
spaincam8 <ccedenotobon47@gmail.com>
2020-04-26 22:15:11 -0400

    random returns one article

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -97,7 +97,7 @@ def get_combined_results(output_A, output_B, weight_A, weight_B):
 
 def get_random(input_data):
     output = []
-    for i in random.sample(range(len(input_data)), 5):
+    for i in random.sample(range(len(input_data)), 1):
         doc_info = {'title': input_data[i].get('title'), 'url':input_data[i].get('url'),
                 'date':input_data[i].get('date'), 'score':input_data[i].get('score'),
                 'source':input_data[i].get('source'), 'summary':input_data[i].get('summary')}
