ae0295b606c5a03d34cfd04ac102d5c0596d7eab
ak778 <ak778@cornell.edu>
2020-05-03 16:53:44 -0700

    imperfect bolding

--- app/static/script.js
+++ app/static/script.js
@@ -1,5 +1,16 @@
-var text = ""
-var i;
-for (i = 0; i < 5; i++) {
-
+// function boldQuery() {
+var divContent = document.getElementById("myDiv").innerHTML;
+var words = query.split(" ")
+for (var i = 0; i < words.length; i++) {
+  divContent = divContent.replace(words[i],"<strong>" + words[i] + "</strong>");
 }
+document.getElementById("myDiv").innerHTML = divContent;
+// }
+
+// function getElements() {
+//   var divContent = document.getElementById("myDiv").innerHTML;
+//   divContent = divContent.replace(/asp.net/gi, "<b>asp.net</b>");
+//   divContent = divContent.replace(/java/gi, "<a style=\"border-bottom-style:dashed;border-bottom-width:thin;text-decoration:none\" rel=\"speechbubble3\" class=\"addspeech\" href=\"#\">java</a>");
+//   divContent = divContent.replace(/perl/gi, "<b>perl</b>");
+//   document.getElementById("myDiv").innerHTML = divContent;
+// }
